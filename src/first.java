75078daa7db6e3110206af18af37a8b620420f5f
Swasti <swastijain16@outlook.com>
2021-09-07 15:08:15 +0530

    variables added

--- src/first.java
+++ src/first.java
@@ -6,6 +6,7 @@ public class first {
        System.out.println("NUM="+num);
        System.out.println("Hello World!");
        num=2* num;
+       System.out.println("Value of num when multiplied by 2=");
        System.out.println(num);
    }
 }
