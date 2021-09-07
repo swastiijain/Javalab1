54e61efdfc694ae6635446de84bafa03b90543c7
Swasti <swastijain16@outlook.com>
2021-09-07 15:06:52 +0530

    variables added

--- src/first.java
+++ src/first.java
@@ -1,2 +1,11 @@
+//First JAVA Program
 public class first {
+   public static void main(String args[])
+   {
+       int num =10;
+       System.out.println("NUM="+num);
+       System.out.println("Hello World!");
+       num=2* num;
+       System.out.println(num);
+   }
 }
